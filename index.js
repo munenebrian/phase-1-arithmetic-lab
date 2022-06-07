3e76514d1439fee7fccffd064b14e73aa82c2933
munenebrian <njerubrianmunene@gmail.com>
2022-06-07 09:48:11 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+const random = Math.ceil(Math.random());
+const num3 = 16;
+const num4 = 4;
+const mod = num3 / num4;
+const max = Math.max(20, 5, 6, 7);
